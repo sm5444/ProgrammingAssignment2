5eee651a9629c93e72dfed7eb4e9aea1d6caeeeb
sm5444 <shreyasm@outlook.com>
2015-03-21 00:16:03 +0530

    commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These fuctions help in calculating the inverse of a non singular square matrix 
+##They also help speeding this process by using cache to store the result of 
+##the previously calculated inverse
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    ##"<<-" operator is used to assign a value to an object in an environment 
+    ## that is different from the current environment
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  ## Following list containes matrix and its inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This computes the inverse of the matrix returned by makeCacheMatrix above. 
+## If the inverse has already been calculated, then the cachesolve retrieves
+## the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
